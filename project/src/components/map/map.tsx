136bf807deae88c8784da6be8c2cf616eb073195
Roman Katrovsky <katrovsky@gmail.com>
2022-08-04 16:24:08 +0300

    демонстрация шаг ...8

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,13 +1,28 @@
 //import leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import {Icon} from 'leaflet';
+
 import { useRef } from 'react';
 import useMap from '../../hooks/useMap';
 
 import {Offer} from '../../types/offer';
+import {URL_MARKER_DEFAULT, URL_MARKER_CURRENT} from '../../const';
 
 type MapProps = {
   offer: Offer;
 }
+
+const defaultCustomIcon = new Icon({
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
+
+const currentCustomIcon = new Icon({
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [40, 40],
+  iconAnchor: [20, 40]
+});
 
 function Map(props: MapProps): JSX.Element {
   const {offer} = props;
@@ -16,7 +31,7 @@ function Map(props: MapProps): JSX.Element {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const map = useMap(mapRef, offer);
 
-  return <div style={{height: '500px'}} ref={mapRef}></div>;
+  return <div style={{height: '100%'}} ref={mapRef}></div>;
 }
 
 export default Map;
--- project/src/const.ts
+++ project/src/const.ts
@@ -20,3 +20,7 @@ export const STARS_RATING: {
     {starsNumber: 2, description: 'baddly'},
     {starsNumber: 1, description: 'terribly'},
   ];
+
+export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
