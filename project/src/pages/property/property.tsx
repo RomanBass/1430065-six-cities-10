8674793ebc5c49761f910085266100fc26a910c5
Roman Katrovsky <katrovsky@gmail.com>
2022-08-11 07:06:44 +0300

    Реализует отображение 4 метки на карте близлежащих предложений, из них одну оранжевую - соответствующую текущей карточке.

--- project/src/pages/property/property.tsx
+++ project/src/pages/property/property.tsx
@@ -144,7 +144,7 @@ function Property({ offers, reviews }: RoomProps): JSX.Element {
             </div>
 
             <section className="property__map map">
-              <Map city={offers[0].city} offers={offers.slice(1)} selectedOffer={undefined}/>
+              <Map city={offers[0].city} offers={offers} selectedOffer={currentOffer}/>
             </section>
 
           </section>
