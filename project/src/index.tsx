60bd4d3f42b8a94d1992cda65bc7c9d79586c1ed
Roman Katrovsky <katrovsky@gmail.com>
2022-10-13 22:33:35 +0300

    p3

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { offers } from './mocks/offers';
 import { reviews } from './mocks/reviews';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -14,10 +16,12 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App
-      rentalOffersNumber={offers.length}
-      offers={offers}
-      reviews={reviews}
-    />
+    <Provider store={store}>
+      <App
+        rentalOffersNumber={offers.length}
+        offers={offers}
+        reviews={reviews}
+      />
+    </Provider>
   </React.StrictMode>,
 );
