5eb434674c5aca7cd835bb5a7b8766a8b84569ef
Roman Katrovsky <katrovsky@gmail.com>
2022-11-05 22:41:17 +0300

    консультация по удалению маркеров

--- project/src/components/map/map.tsx
+++ project/src/components/map/map.tsx
@@ -1,5 +1,5 @@
 import 'leaflet/dist/leaflet.css';
-import { Icon, Marker } from 'leaflet';
+import { Icon, Marker, LayerGroup } from 'leaflet';
 import { useEffect, useRef } from 'react';
 import useMap from '../../hooks/useMap';
 import { Offer } from '../../types/offer';
@@ -22,53 +22,46 @@ const currentCustomIcon = new Icon({
   iconAnchor: [14, 40]
 });
 
+const defaultCity = {
+  location: {
+    latitude: 52.370216,
+    longitude: 4.895168,
+    zoom: 10,
+  },
+  name: 'Amsterdam',
+};
+
 function Map(props: MapProps): JSX.Element {
   const { selectedOffer } = props;
   const mapRef = useRef(null);
 
   const offersBySelectedCity = useAppSelector((state) => state.offersList);
-
-  const map = useMap(mapRef, offersBySelectedCity[0].city);
-  const emptyMarker = new Marker({
-    lat: 0,
-    lng: 0,
-  });
-  const markersRef = useRef([{currentMarker: emptyMarker, currentCity: offersBySelectedCity[0].city.name}]);
+  const city = offersBySelectedCity[0]?.city || defaultCity;
+  const map = useMap(mapRef, city);
 
   useEffect(() => {
-
+    const layerMarkers = new LayerGroup();
     if (map) {
 
-      map.panTo([offersBySelectedCity[0].city.location.latitude, offersBySelectedCity[0].city.location.longitude]);
+      map.panTo([city.location.latitude, city.location.longitude]);
 
-      offersBySelectedCity.forEach((property) => {
+      offersBySelectedCity.forEach((place) => {
+
         const marker = new Marker({
-          lat: property.location.latitude,
-          lng: property.location.longitude,
+          lat: place.location.latitude,
+          lng: place.location.longitude,
         });
 
-        marker.setIcon(selectedOffer !== undefined && property.id === selectedOffer.id
+        marker.setIcon(selectedOffer !== undefined && place.id === selectedOffer.id
           ? currentCustomIcon
           : defaultCustomIcon)
-          .addTo(map);
-
-        markersRef.current.push({currentMarker: marker, currentCity: offersBySelectedCity[0].city.name});
-
-        let prevMarkers = markersRef.current.filter((elem) => elem.currentCity !== offersBySelectedCity[0].city.name);
-
-        prevMarkers.forEach((prevMarker) => {
-          map.removeLayer(prevMarker.currentMarker);
-          markersRef.current = markersRef.current.filter((elem) => elem.currentCity !== prevMarker.currentCity);
-        });
-
-        prevMarkers = [];
-
-        //eslint-disable-next-line no-console
-        console.log(markersRef.current);
+          .addTo(layerMarkers);
 
       });
+      layerMarkers.addTo(map);
     }
-  }, [map, offersBySelectedCity, selectedOffer]);
+    return () => {layerMarkers.remove();};
+  }, [city, map, offersBySelectedCity, selectedOffer]);
 
   return <div style={{ height: '100%' }} ref={mapRef}></div>;
 }
