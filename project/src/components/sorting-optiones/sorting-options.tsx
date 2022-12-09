741df537d496a05f602c2eab640db69ed6b8754d
Roman Katrovsky <katrovsky@gmail.com>
2022-12-09 22:42:31 +0300

    добавлен пробел в Sort by

--- project/src/components/sorting-optiones/sorting-options.tsx
+++ project/src/components/sorting-optiones/sorting-options.tsx
@@ -23,7 +23,7 @@ function SortingOptions(): JSX.Element {
 
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-caption">Sort by </span>
       <span className="places__sorting-type" tabIndex={0}>
         {activeSortingOption}
         <svg className="places__sorting-arrow" width="7" height="4">
