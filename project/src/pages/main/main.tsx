e3477f055249bb748df662cbe4f7c2c2a0a22a3f
Roman Katrovsky <katrovsky@gmail.com>
2023-01-12 22:49:49 +0300

    Реализует п.6 задания

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -6,6 +6,7 @@ import { Offer } from '../../types/offer';
 import CitiesList from '../../components/cities-list/cities-list';
 import SortingOptions from '../../components/sorting-optiones/sorting-options';
 import { useAppSelector } from '../../hooks';
+import { AuthorizationStatus } from '../../const';
 
 function Main(): JSX.Element {
 
@@ -19,6 +20,41 @@ function Main(): JSX.Element {
 
   const offersBySelectedCity = useAppSelector((state) => state.offersList);
   const activeCity = useAppSelector((state) => state.activeCity);
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+
+  const renderSignInArea = () => {
+    if (authorizationStatus === AuthorizationStatus.Auth) {
+      return (
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <a className="header__nav-link header__nav-link--profile" href="/">
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+              <span className="header__favorite-count">3</span>
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <a className="header__nav-link" href="/">
+              <span className="header__signout">Sign out</span>
+            </a>
+          </li>
+        </ul>
+      );
+    } else {
+      return (
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <a className="header__nav-link header__nav-link--profile" href="/">
+              <div className="header__avatar-wrapper user__avatar-wrapper">
+              </div>
+              <span className="header__login">Sign in</span>
+            </a>
+          </li>
+        </ul>
+      );
+    }
+  };
 
   return (
     <div className="page page--gray page--main">
@@ -27,21 +63,7 @@ function Main(): JSX.Element {
           <div className="header__wrapper">
             <Logo />
             <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="/">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="/">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
+              {renderSignInArea()}
             </nav>
           </div>
         </div>
